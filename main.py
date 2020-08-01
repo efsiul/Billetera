264e7007571e568d3a816c0131efb478ee7b1989
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2020-08-01 16:06:48 -0500

    Codigo inicial adjuntado

---
+++ main.py
@@ -0,0 +1,79 @@
+
+import os
+from csv import *
+from datetime import *
+
+
+def run():
+    
+    now = datetime.now()
+    fecha = now.strftime('%d/%m/%Y Hora: %H:%M:%S')
+    while True:
+        
+            option = input('''
+                        |***********************************************************************************|
+                        |                           BIENVENIDO A SU BILLETERA                               |
+                        |                                                                                   |
+                        |   Elija una de las siguientes opciones, digitando el número correspondiente:      |
+                        |                                                                                   |
+                        |   1 - Recibir cantidad                                                            |
+                        |   2 - Transferir monto                                                            |
+                        |   3 - Mostrar balance de moneda                                                   |
+                        |   4 - Mostrar balance general                                                     |
+                        |   5 - Mostrar historico de transacciones                                          |
+                        |   6 - Salir del programa                                                          |
+                        |***********************************************************************************|
+                        \n''')
+            
+            if option == '1':
+                print('''
+                        |*********************************************|
+                        |             RECIBIR CANTIDAD                |         
+                        |*********************************************|
+                    \n''')
+                coin = str(input("Indique la moneda que va a recibir: "))
+                count = float(input('Indique la cantidad de dinero que desea recibir: '))
+                code = input('Indique el codigo de quien envia: ')
+                
+                os.system("pause")
+                
+            elif option == '2':
+                print('''
+                        |*********************************************|
+                        |             TRANSFERIR MONTO                |         
+                        |*********************************************|
+                    \n''')
+                os.system("pause")
+                
+            elif option == '3':
+                print('''
+                        |*********************************************|
+                        |         MOSTRAR BALANCE DE MONEDA           |         
+                        |*********************************************|
+                    \n''')
+                os.system("pause")
+            elif option == '4':
+                print('''
+                        |*********************************************|
+                        |           MOSTRAR BALANCE GENERAL           |         
+                        |*********************************************|
+                    \n''')
+                os.system("pause")
+            elif option == '5':
+                print('''
+                        |*********************************************|
+                        |     MOSTRAR HISTORICO DE TRANSACCIONES      |        
+                        |*********************************************|
+                    \n''')
+                
+                os.system("pause")
+            elif option == '6':
+                print('Saliendo...')
+                break
+            else:
+                print("OPCIÓN INVALIDA, MARQUE UNA DE LAS OPCIONES DEL MENÚ")
+                os.system("pause")
+                
+
+if __name__ == '__main__':
+    run()
