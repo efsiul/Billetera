dbe4dd59f7f304d9900476f13fb1e13578c02a91
Luis Felipe Cadavid <felipe.cadavid@udea.edu.co>
2020-08-07 21:23:48 -0500

    Se organiza el gardado dy lectura de informacion

--- main.py
+++ main.py
@@ -7,9 +7,20 @@ def run():
     transaction = Transactions()
     now = datetime.now()
     fecha = now.strftime('%d/%m/%Y Hora: %H:%M:%S')
+    
+    with open('transacciones.csv', 'r') as f:
+        reader = csv.reader(f)
+        for idx, row in enumerate(reader):
+            if idx == 0:
+                continue
+            try:                                                                                                #Como los datos se guardan en el csv dejando una liena de espacio, al buscar en row[0],row[1],row[2]....row[8] sacara error
+                transaction.addTransaction(row[0], row[1], row[2],row[3],row[4],row[5],row[6],row[7],row[8])     #por lo que la linea hay una lista vacia y no hay indices en el la lista row. Por ello se genera una excepción  
+            except IndexError:                                                                                  #haciendo que el programa siga, y salte a la siguiente linea.
+                continue
+    
     while True:
         
-            option = input('''
+        option = input('''
                         |***********************************************************************************|
                         |                           BIENVENIDO A SU BILLETERA                               |
                         |                                                                                   |
@@ -24,64 +35,59 @@ def run():
                         |***********************************************************************************|
                         \n''')
             
-            if option == '1':
-                print('''
+        if option == '1':
+            print('''
                         |*********************************************|
                         |             RECIBIR CANTIDAD                |         
                         |*********************************************|
                     \n''')
-                coin = str(input("Indique la moneda que va a recibir: "))
-                count = float(input('Indique la cantidad de dinero que desea recibir: '))
-                code = input('Indique el codigo de quien envia: ')
-                transaction.typeTransaction(code, "receives", fecha, coin, count)
+            coin = str(input("Indique la moneda que va a recibir: "))
+            count = float(input('Indique la cantidad de dinero que desea recibir: '))
+            code = input('Indique el codigo de quien envia: ')
+            transaction.typeTransaction(code, "receives", fecha, coin, count)
                 
-                os.system("pause")
+            os.system("pause")
                 
-            elif option == '2':
-                print('''
+        elif option == '2':
+            print('''
                         |*********************************************|
                         |             TRANSFERIR MONTO                |         
                         |*********************************************|
                     \n''')
                 
+            transaction._accomulator(1)
+            os.system("pause")
                 
-                os.system("pause")
-                
-            elif option == '3':
-                print('''
+        elif option == '3':
+            print('''
                         |*********************************************|
                         |         MOSTRAR BALANCE DE MONEDA           |         
                         |*********************************************|
                     \n''')
-                os.system("pause")
-            elif option == '4':
-                print('''
+            os.system("pause")
+        elif option == '4':
+            print('''
                         |*********************************************|
                         |           MOSTRAR BALANCE GENERAL           |         
                         |*********************************************|
                     \n''')
-                os.system("pause")
-            elif option == '5':
-                print('''
+            os.system("pause")
+        elif option == '5':
+            print('''
                         |*********************************************|
                         |     MOSTRAR HISTORICO DE TRANSACCIONES      |        
                         |*********************************************|
                     \n''')
-                transaction.show_all()
-                os.system("pause")
+            transaction.show_all()
+            os.system("pause")
                 
-            elif option == '6':
-                print('Saliendo...')
-                break
-            else:
-                print("OPCIÓN INVALIDA, MARQUE UNA DE LAS OPCIONES DEL MENÚ")
-                os.system("pause")
+        elif option == '6':
+            print('Saliendo...')
+            break
+        else:
+            print("OPCIÓN INVALIDA, MARQUE UNA DE LAS OPCIONES DEL MENÚ")
+            os.system("pause")
                 
 
-if __name__ == '__main__':
-    with open('transacciones.csv', 'r') as f:
-        reader = csv.reader(f)
-        for idx, row in enumerate(reader):
-            if idx == 0:
-                continue  
+if __name__ == '__main__':  
     run()
--- transaction.py
+++ transaction.py
@@ -17,7 +17,6 @@ class Transaction:
 
 
 class Transactions:
-    var=0
     def __init__(self):
         self._transactions=[]
         
@@ -29,7 +28,6 @@ class Transactions:
         
         monto_accom=float(self._accomulator(8))
         num_Transaccion=int(self._accomulator(0))
-        
         if typeT=="receives":
             mont=monto_accom+valor
             transa=num_Transaccion+1
@@ -37,19 +35,19 @@ class Transactions:
         elif typeT=="send":
             mont=self.monto_accom+((-1.0)*valor)
             transa=self.num_Transaccion+1
-            
+        
+        self.addTransaction(transa, typeT, code, date, coin, precio, count, valor, mont)
+        
+    def addTransaction(self, transa, typeT, code, date, coin, precio, count, valor, mont):
         transaction=Transaction(transa, typeT, code, date, coin, precio, count, valor, mont)
         self._transactions.append(transaction)
         self._save()
     
     def _accomulator(self,i):
-        global var
         lis= list(csv.reader(open('transacciones.csv', 'r')))
-        end_Line=lis[-1] # prints final line as a lis
-        for row in end_Line:
-            var=str(row[i])
-        return var
-    
+        lon=(len(lis))
+        return lis[lon-2][i]
+        
     def show_all(self):
         for i in self._transactions:
             self._print_transaction(i)
@@ -63,7 +61,7 @@ class Transactions:
         print('Moneda: {}'.format(transaction.coin))
         print('Precio de la moneda: {}'.format(transaction.price))
         print('cantidad a operar: {}'.format(transaction.count))
-        print('valor de operado {}'.format(transaction.value))
+        print('valor operado {}'.format(transaction.value))
         print('Monto a la fecha {}'.format(transaction.amount))
         print('*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*----*')
         
@@ -80,7 +78,8 @@ class Transactions:
         print('************************************')
         print('Transacción no encontrada')
         print('************************************')
-            
+    
+    
     def _save(self):
         with open('transacciones.csv','w') as f:
             writer=csv.writer(f)   
